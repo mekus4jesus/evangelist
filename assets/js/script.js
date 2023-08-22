7c6f000c51966b1170d2b2faca98ede74064c069
The Evangelist <138933915+mekus4jesus@users.noreply.github.com>
2023-08-22 06:46:53 +0100

    Update script.js
    a slight change

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 var typed = new Typed('.typing', {
-    strings: ["Web Designer","Web Developer","UI/UX Designer","Clergy"],
+    strings: ["a Web Designer","a Web Developer","an UI/UX Designer","a Clergy"],
     typeSpeed: 250,
     backSpeed: 150,
     loop: true
